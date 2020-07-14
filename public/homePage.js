128e28d57ba8304ac9fb6d157cf9ef88cd46c5dd
Leonid Kostyuchenko <kstchnko@gmail.com>
2020-07-15 02:57:41 +0300

    login, register and logout fixed

---
+++ public/homePage.js
@@ -0,0 +1,4 @@
+'use strict';
+
+const logout = new LogoutButton;
+logout.action = () => ApiConnector.logout(response => {if (response.success) {location.reload()}});
---
+++ public/loginPage.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const login = new UserForm;
+login.loginFormCallback = data => ApiConnector.login(data, response => response.success ? location.reload() : userForm.setLoginErrorMessage(response.data));
+login.registerFormCallback = data => ApiConnector.register(data, response => response.success ? location.reload() : userForm.setRegisterErrorMessage(response.data));

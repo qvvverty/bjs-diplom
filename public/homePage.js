064c590d99bcf12b849da30c943931e589176f34
Leonid Kostyuchenko <kstchnko@gmail.com>
2020-07-17 02:54:24 +0300

    user profile and rates board fixed

--- public/homePage.js
+++ public/homePage.js
@@ -2,3 +2,24 @@
 
 const logout = new LogoutButton;
 logout.action = () => ApiConnector.logout(response => {if (response.success) {location.reload()}});
+
+ApiConnector.current(response => {
+  if (response) {
+    ProfileWidget.showProfile(response.data);
+  }
+});
+
+const rates = new RatesBoard;
+function getRates() {
+  // console.log('Запрос данных...');
+  ApiConnector.getStocks(response => {
+    if (response.success) {
+      rates.clearTable();
+      rates.fillTable(response.data);
+    }
+  });
+}
+
+getRates();
+setInterval(getRates, 60000);
+
